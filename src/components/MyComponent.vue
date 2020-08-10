abfd1a048f45c3ae48f6a86cd7d8ee17117beca8
Nickolas cecilio <nickolas.cecilio@stormsec.com.br>
2020-08-10 17:19:43 -0300

    first commit

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="content">
+    <div class="first-content">
+      <img class="icon-test" src="https://travelpedia.com.br/wp-content/uploads/2017/11/pizza-icon-300x300.png" />
+      <div class="text-main">João Capelletto</div>
+    </div>
+    <div class="second-content">
+      <h4>Sem tempo, irmão</h4>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyComponent",
+  props: {
+  }
+};
+</script>
+<style scoped lang="scss">
+.content {
+
+  .first-content {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    .icon-test {
+      width: 100px;
+    }
+
+    .text-main {
+      font-size: 4em;
+    }
+  }
+
+  .second-content {
+    font-weight: 100;
+    font-size: 1em;
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <MyComponent></MyComponent>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import MyComponent from "@/components/MyComponent.vue";
+
+export default {
+  name: "Home",
+  components: {
+    MyComponent
+  }
+};
+</script>
+
