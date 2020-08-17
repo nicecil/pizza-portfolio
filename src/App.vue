9737387272940175c76ec0a424d0fa464fd5c702
Nickolas cecilio <nickolas.cecilio@stormsec.com.br>
2020-08-17 01:07:01 -0300

    implemented app bar with home and about page links

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <v-app>
+    <AppBar></AppBar>
     <v-main>
-      <Home></Home>
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import Home from "./views/Home";
+import AppBar from "./components/AppBar";
 
 export default {
   name: "App",
 
   components: {
-    Home
+    AppBar
   },
 
   data: () => ({
---
+++ src/components/AppBar.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-app-bar app elevation="4">
+        <v-spacer></v-spacer>
+        <v-btn text @click="$router.push('/')">Home</v-btn>
+        <v-btn text @click="$router.push('/about')">About</v-btn>
+    </v-app-bar>
+</template>
+
+<script>
+    export default {
+        name: "AppBar"
+    }
+</script>
+
+<style scoped>
+
+</style>
